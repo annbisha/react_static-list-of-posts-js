3b2544dbfd3d3481805754694f857e7dac5dd052
annbisha <annskaylove@gmail.com>
2026-05-28 12:35:14 +0300

    d

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,14 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="post">
+    <h2 className="post__title">{post.title}</h2>
+
+    <p className="post__body">{post.body}</p>
+
+    <UserInfo user={post.user} />
+
+    <CommentList comments={post.comments} />
+  </div>
+);
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,9 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => (
+  <div className="UserInfo">
+    <strong className="UserInfo__name">{user.name}</strong>
+
+    <a className="UserInfo__email" href={`mailto:${user.email}`}>
+      {user.email}
+    </a>
+  </div>
+);
